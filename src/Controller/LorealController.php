97368d5bb428b184e6069370fe0914a03065c624
guihomeWebDev <lebot.guihome@gmail.com>
2023-02-28 16:49:03 +0100

    add tome slots to export csv

--- src/Controller/LorealController.php
+++ src/Controller/LorealController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\CalendarRepository;
 use App\Repository\CustomerRepository;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -47,15 +48,28 @@ class LorealController extends AbstractController
     }
 
     #[Route('/loreal/export', name: 'app_loreal_export')]
-    public function export(CalendarRepository $calendarRepository, CustomerRepository $customerRepository): Response
+    public function export(CalendarRepository $calendarRepository, CustomerRepository $customerRepository, Request $request): Response
     {
-        $customer = $customerRepository->findOneBy(['name' => 'Loreal']);
-        $calendarEvents = $calendarRepository->findBy(['customer' => $customer]);
-    
+        // Get L'Oréal customer object
+        $lorealCustomer = $customerRepository->findOneBy(['name' => 'Loreal']);
+
+        // Get start and end dates from request parameters
+        $startDate = $request->query->get('start_date');
+        $endDate = $request->query->get('end_date');
+
+        // Convert start and end dates to DateTimeInterface objects
+        $startDate = \DateTime::createFromFormat('Y-m-d', $startDate);
+        $endDate = \DateTime::createFromFormat('Y-m-d', $endDate);
+
+        // Find calendar events for L'Oréal customer in date range
+        if($lorealCustomer){
+            $calendarEvents = $calendarRepository->findLorealEventsByDate($startDate, $endDate);
+
+        }
         // Create a new Spreadsheet object
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
-    
+
         // Set headers
         $sheet->setCellValue('A1', 'Type');
         $sheet->setCellValue('B1', 'Date arrivée / départ prévu');
@@ -63,40 +77,39 @@ class LorealController extends AbstractController
         $sheet->setCellValue('D1', 'Nbr de palettes');
         $sheet->setCellValue('E1', 'Contenu du camion');
         $sheet->setCellValue('F1', 'Date de contrôle marchandises');
-        $sheet->setCellValue('G1', 'Client');
-    
+
         // Set data
         $rowIndex = 2;
         foreach ($calendarEvents as $event) {
-            $sheet->setCellValue('A' . $rowIndex, $event->getTitle());
-            $sheet->setCellValue('B' . $rowIndex, $event->getStart()->format('d-m-Y H:i:s'));
-            $sheet->setCellValue('C' . $rowIndex, $event->getCommandNumber());
-            $sheet->setCellValue('D' . $rowIndex, $event->getPalletsNumber());
-            $sheet->setCellValue('E' . $rowIndex, $event->getContentTruck());
-            $sheet->setCellValue('F' . $rowIndex, $event->getCheckedAt());
-            $sheet->setCellValue('G' . $rowIndex, $event->getCustomer()->getName());
-            $rowIndex++;
+            if ($event->getCustomer()->getName() == 'Loreal') {
+                $sheet->setCellValue('A' . $rowIndex, $event->getTitle());
+                $sheet->setCellValue('B' . $rowIndex, $event->getStart()->format('d-m-Y H:i:s'));
+                $sheet->setCellValue('C' . $rowIndex, $event->getCommandNumber());
+                $sheet->setCellValue('D' . $rowIndex, $event->getPalletsNumber());
+                $sheet->setCellValue('E' . $rowIndex, $event->getContentTruck());
+                $sheet->setCellValue('F' . $rowIndex, $event->getCheckedAt());
+                $rowIndex++;
+            }
         }
-    
+
         // Create a new CSV writer object
         $writer = new \PhpOffice\PhpSpreadsheet\Writer\Csv($spreadsheet);
-    
+
         // Set the output file name
         $fileName = 'loreal_planning.csv';
-    
+
         // Create the response object
         $response = new Response();
-    
+
         // Set the response headers
         $response->headers->set('Content-Type', 'text/csv');
         $response->headers->set('Content-Disposition', 'attachment; filename="' . $fileName . '"');
-    
+
         // Set the content of the response to the CSV file
         ob_start();
         $writer->save('php://output');
         $csvContent = ob_get_clean();
         $response->setContent($csvContent);
-    
         return $response;
     }
 }
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -23,27 +23,27 @@ class MainController extends AbstractController
     #[Route('/calendar/view', name: 'app_calendar')]
     public function calendar(CalendarRepository $calendarRepository, CustomerRepository $customerRepository): Response
     {
-        // if ($this->isGranted('ROLE_ADMIN')) {
-        //     $events = $calendarRepository->findAll();
+        if ($this->isGranted('ROLE_ADMIN')) {
+            $events = $calendarRepository->findAll();
 
-        //     $rdvs = [];
+            $rdvs = [];
 
-        //     foreach($events as $event){
-        //         $rdvs[] = [
-        //             'id' => $event->getId(),
-        //             'title' => $event->getTitle(),                
-        //             'backgroundColor'=> $event->getBackgroundColor(),
-        //             'start' => $event->getStart()->format('Y-m-d H:i:s'),
-        //             'end' => $event->getEnd()->format('Y-m-d H:i:s'),
-        //             'building' => $event->getBuilding()->getName(),
-        //             'customer' => $event->getCustomer()->getName(),
-        //             'palletsNumber' => $event->getPalletsNumber(),
-        //         ];
-        //     }
-        //     $data = json_encode($rdvs);
+            foreach($events as $event){
+                $rdvs[] = [
+                    'id' => $event->getId(),
+                    'title' => $event->getTitle(),                
+                    'backgroundColor'=> $event->getBackgroundColor(),
+                    'start' => $event->getStart()->format('Y-m-d H:i:s'),
+                    'end' => $event->getEnd()->format('Y-m-d H:i:s'),
+                    'building' => $event->getBuilding()->getName(),
+                    'customer' => $event->getCustomer()->getName(),
+                    'palletsNumber' => $event->getPalletsNumber(),
+                ];
+            }
+            $data = json_encode($rdvs);
 
-        //     return $this->render('main/index.html.twig', compact('data'));
-        // }
+            return $this->render('main/index.html.twig', compact('data'));
+        }
         if ($this->isGranted('ROLE_LOREAL')) {
             $customer = $customerRepository->findOneBy(['name' => 'Loreal']);
             $events = $calendarRepository->findBy(['customer' => $customer]);
@@ -111,15 +111,23 @@ class MainController extends AbstractController
     }
 
     #[Route('/calendar/building/export', name: 'app_building_export')]
-    public function export(CalendarRepository $calendarRepository, CustomerRepository $customerRepository): Response
+    public function export(CalendarRepository $calendarRepository, CustomerRepository $customerRepository, Request $request): Response
     {
-        $customer = $customerRepository->findAll();
-        $calendarEvents = $calendarRepository->findAll();
-    
+        // Get start and end dates from request parameters
+        $startDate = $request->query->get('start_date');
+        $endDate = $request->query->get('end_date');
+
+        // Convert start and end dates to DateTimeInterface objects
+        $startDate = \DateTime::createFromFormat('Y-m-d', $startDate);
+        $endDate = \DateTime::createFromFormat('Y-m-d', $endDate);
+
+        // Find calendar events in date range
+        $calendarEvents = $calendarRepository->findByDateRange($startDate, $endDate);
+            
         // Create a new Spreadsheet object
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
-    
+        
         // Set headers
         $sheet->setCellValue('A1', 'Type');
         $sheet->setCellValue('B1', 'Date arrivée / départ prévu');
@@ -128,7 +136,7 @@ class MainController extends AbstractController
         $sheet->setCellValue('E1', 'Contenu du camion');
         $sheet->setCellValue('F1', 'Date de contrôle marchandises');
         $sheet->setCellValue('G1', 'Client');
-    
+        
         // Set data
         $rowIndex = 2;
         foreach ($calendarEvents as $event) {
@@ -141,26 +149,26 @@ class MainController extends AbstractController
             $sheet->setCellValue('G' . $rowIndex, $event->getCustomer()->getName());
             $rowIndex++;
         }
-    
+        
         // Create a new CSV writer object
         $writer = new \PhpOffice\PhpSpreadsheet\Writer\Csv($spreadsheet);
-    
+        
         // Set the output file name
         $fileName = 'Qualiservice_planning.csv';
-    
+        
         // Create the response object
         $response = new Response();
-    
+        
         // Set the response headers
         $response->headers->set('Content-Type', 'text/csv');
         $response->headers->set('Content-Disposition', 'attachment; filename="' . $fileName . '"');
-    
+        
         // Set the content of the response to the CSV file
         ob_start();
         $writer->save('php://output');
         $csvContent = ob_get_clean();
         $response->setContent($csvContent);
-    
+        
         return $response;
     }
 }
